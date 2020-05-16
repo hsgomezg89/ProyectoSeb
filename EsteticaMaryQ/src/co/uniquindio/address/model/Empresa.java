efcd34f6b9b1a533c0d5356921ecc5765ed0f8e8
hsgomezg89 <44373608+hsgomezg89@users.noreply.github.com>
2020-05-15 20:59:21 -0500

    Eliminar linea
    Se elimina linea de codigo sin uso

--- EsteticaMaryQ/src/co/uniquindio/address/model/Empresa.java
+++ EsteticaMaryQ/src/co/uniquindio/address/model/Empresa.java
@@ -3,13 +3,13 @@ package co.uniquindio.address.model;
 import java.util.ArrayList;
 
 public class Empresa {
-	
+
 	private Ciudad[]ciudades;
 	private ArrayList<Sede>Sedes;
 	private Administrador administrador;
-	private 
-	
-	
-	
+
+
+
+
 
 }
